f56451b1667b5f5c4fb31c4bf4d8d55c39f03105
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-01-07 21:36:40 +0600

    Add files via upload

---
+++ LeetCode/1. Two Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> result;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    
+                    return result;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
