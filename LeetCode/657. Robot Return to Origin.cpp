b6d9d768b4fb5cf5580a3fa099d1291fefa41783
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-01-22 22:25:03 +0600

    Add files via upload

---
+++ LeetCode/657. Robot Return to Origin.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;        
+        for(int i = 0; i < moves.length(); i++){
+            if(moves[i] == 'R')
+                x+=1;    
+            else if(moves[i] == 'L')
+                x-=1;
+            else if(moves[i] == 'U')
+                y+=1;    
+            else if(moves[i] == 'D')
+                y-=1;
+        }
+        return (x == 0 && y == 0);
+    }
+};
