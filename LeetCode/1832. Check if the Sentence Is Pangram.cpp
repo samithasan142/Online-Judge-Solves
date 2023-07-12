dc7cfbea4f4ee0fdc5d309da58ff3199cf88a75b
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-07-12 21:47:39 +0600

    Update 1832. Check if the Sentence Is Pangram.cpp

--- LeetCode/1832. Check if the Sentence Is Pangram.cpp
+++ LeetCode/1832. Check if the Sentence Is Pangram.cpp
@@ -7,6 +7,5 @@ public:
 
 /*
 The unordered_set is a container that stores unique elements in no particular order.
-
 The code creates an unordered set of unique characters from the sentence and checks if the size of the set is equal to 26 to determine if the sentence is a pangram or not.
 */
