33606d0d6ddbfcdcc70a1152e646efb5b23bde56
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-07-13 13:30:29 +0600

    Add files via upload

---
+++ LeetCode/69. Sqrt(x).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long left = 0, right = x, ans=0;
+
+        while(left <= right){
+            long long mid = left + (right-left) / 2;
+            if(mid*mid <= x){
+                ans = mid;
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
