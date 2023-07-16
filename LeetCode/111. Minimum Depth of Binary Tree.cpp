d0ebb35adc94f4f109d3b36920a5c001379b3f43
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-07-17 00:27:10 +0600

    Update 111. Minimum Depth of Binary Tree.cpp

--- LeetCode/111. Minimum Depth of Binary Tree.cpp
+++ LeetCode/111. Minimum Depth of Binary Tree.cpp
@@ -6,10 +6,12 @@ public:
         
         int leftDepth = minDepth(root->left);
         int rightDepth = minDepth(root->right);
-        
+
+        // If either child is NULL, return the depth of the non-null child + 1
         if (root->left == NULL || root->right == NULL)
             return max(leftDepth, rightDepth) + 1;
-        
+
+        // Both children are not NULL, return the minimum depth among them + 1
         return min(leftDepth, rightDepth) + 1;
     }
 };
