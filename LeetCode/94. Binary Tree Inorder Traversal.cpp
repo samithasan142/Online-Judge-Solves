53de576e7f24392e7b8d886b3b7a31edf6efc3a0
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-07-17 16:49:44 +0600

    Update 94. Binary Tree Inorder Traversal.cpp

--- LeetCode/94. Binary Tree Inorder Traversal.cpp
+++ LeetCode/94. Binary Tree Inorder Traversal.cpp
@@ -1,3 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * };
+ */
+
 class Solution {
 public:
     vector<int> ans;
@@ -18,3 +28,28 @@ public:
         return ans;
     }
 };
+
+/*
+int main() {
+    // Create the binary tree
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    // Create an instance of the Solution class
+    Solution solution;
+
+    // Perform the inorder traversal
+    vector<int> result = solution.inorderTraversal(root);
+
+    cout << "Inorder Traversal: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+*/
