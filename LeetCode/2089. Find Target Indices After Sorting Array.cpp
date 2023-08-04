eb471e2df79c2d3a198ef91dd1e669e2589beef8
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-04 12:55:15 +0600

    Update 2089. Find Target Indices After Sorting Array.cpp

--- LeetCode/2089. Find Target Indices After Sorting Array.cpp
+++ LeetCode/2089. Find Target Indices After Sorting Array.cpp
@@ -1,3 +1,8 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+*/
+
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
@@ -11,3 +16,24 @@ public:
         return result;
     }
 };
+
+/*
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1,2,5,2,3};
+    int target = 2;
+    vector<int> result = sol.targetIndices(nums, target);
+
+    cout << "Result: [";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+        if (i < result.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
+*/
