bb47ec51d1235fc9677ae62e70ee461a201ca35b
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-09 22:08:23 +0600

    Update 509. Fibonacci Number.cpp

--- LeetCode/509. Fibonacci Number.cpp
+++ LeetCode/509. Fibonacci Number.cpp
@@ -1,3 +1,8 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+*/
+
 class Solution {
 public:
     int fib(int n) {
@@ -7,3 +12,17 @@ public:
         return fib(n - 1) + fib(n - 2);
     }
 };
+
+/*
+int main() {
+    Solution solution;
+
+    int n = 10;
+    cout << "Fibonacci series up to " << n << ":\n";
+    for (int i = 0; i <= n; i++) {
+        cout << solution.fib(i) << " ";
+    }
+
+    return 0;
+}
+*/
