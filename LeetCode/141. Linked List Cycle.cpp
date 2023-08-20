0e871adaf7391891a938879a802501f6e7cdb47b
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-21 01:22:39 +0600

    Add files via upload

---
+++ LeetCode/141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// If the linked list contains a cycle, the fastPtr will eventually catch up to the slowPtr. If there is no cycle, the fastPtr will reach the end of the list (i.e., fastPtr.next is null or fastPtr is null).
