c51024affc7d09f912c14c82cfeca8b895251b6e
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2023-08-24 00:48:55 +0600

    Add files via upload

---
+++ LeetCode/217. Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(mp[nums[i]] > 1) return true;
+        }
+        return false;
+    }
+};
+
+/*
+	Complexity:
+	Time complexity: O(n)
+	Space complexity: O(n)
+*/
---
+++ LeetCode/53. Maximum Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currentSum = 0;
+        
+        for (int i=0; i<nums.size(); i++) {
+            currentSum += nums[i];
+            maxSum = max(maxSum, currentSum);
+            
+            if (currentSum < 0) currentSum = 0;
+        }   
+        return maxSum;
+    }
+};
